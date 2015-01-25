d1aef1f24401f2853b5b425300e4af5efe9fa091
Rokas <juozapavicius.rokas@gmail.com>
2015-01-25 20:38:57 +0200

    Create prog4_8.sh

---
+++ prog4_8.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+#SBATCH -p short
+#SBATCH -n8
+#SBATCH -C beta
+mpicc -o prog4 prog4.c
+mpirun prog4 -249 250 0.0000000000001
+mpirun prog4 -499 500 0.0000000000001
+mpirun prog4 -249 250 0.00000000000001
+mpirun prog4 -499 500 0.00000000000001
